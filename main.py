831186983b279e183f2c23980544d5bb77d8192c
Jahnavi Gogia <jahnavigogia02@gmail.com>
2024-01-25 01:39:37 +0530

    Initial commmit

---
+++ main.py
@@ -0,0 +1,32 @@
+import cv2
+
+# Load the pre-trained Haarcascades classifier
+face_cascade = cv2.CascadeClassifier('/Users/jahnavi/PycharmProjects/Face-Recoginition-System/deep-learning-model.xml')
+
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read a frame from the webcam
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+    # Draw rectangles around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # Display the frame
+    cv2.imshow('Face Recognition', frame)
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam and close the OpenCV window
+cap.release()
+cv2.destroyAllWindows()
