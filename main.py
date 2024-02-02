624fe9ffc52eed838bc75260494ca0ef4fe57feb
Jahnavi Gogia <jahnavigogia02@gmail.com>
2024-02-03 00:30:40 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from typing import Any
-
+import os
 import cv2
 import glob
+from emailing import send_email
 
 
+status_list = []
 count = 1
 
 # Load the pre-trained Haarcascades classifier
 face_cascade = cv2.CascadeClassifier('/Users/jahnavi/PycharmProjects/Face-Recoginition-System/deep-learning-model.xml')
 
+
+def clean_folder():
+    print("Clean folder function started.")
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+    print("Clean folder function completed.")
+
+
 # Initialize the webcam
 cap = cv2.VideoCapture(0)
 
+
 while True:
+    status = 0
     # Read a frame from the webcam
     ret, frame = cap.read()
 
@@ -30,6 +43,15 @@ while True:
             status = 1
             cv2.imwrite(f"images/{count}.png", frame)
             count = count + 1
+            all_im = glob.glob("images/*.png")
+            index = int(len(all_im) / 2)
+            im_obj = all_im[index]
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email(im_obj)
+        clean_folder()
 
     # Display the frame
     cv2.imshow('Face Recognition', frame)
